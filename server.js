514624f7860bf66dfbc6aff93058ca7c4c310af0
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-18 21:30:59 -0300

    update app.get

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
+//Criando um servidor com Express
 import express from "express";
 
 const app = express();
 
 app.listen(3000, () => {
+
     console.log("Server is running on port 3000");
-})
+});
+
+//Pode deixar vazio, só  com / ou colocar algo como /api
+app.get("/api", (req, res) => {
+
+    res.status(200).send("Hello World!");
+});
