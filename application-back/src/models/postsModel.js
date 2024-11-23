d34a0b9ae3811bf3129c78c28488a91482a03f71
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-23 18:43:39 -0300

    add conectarAoBanco

---
+++ application-back/src/models/postsModel.js
@@ -0,0 +1,12 @@
+import conectarAoBanco from "../config/dbConfig.js";
+
+const conexao = await conectarAoBanco(process.env.STRING_CONEXAO)
+
+export default async function getTodosPosts() {
+
+    const db = conexao.db("real-world-application");
+
+    const colecao = db.collection("posts");
+
+    return colecao.find().toArray();
+}
