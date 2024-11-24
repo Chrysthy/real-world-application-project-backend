2ff27b652d15e702c93f46936c6dcc1bf00c370a
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-24 18:09:46 -0300

    add const storage

--- application-back/src/routes/postsRoutes.js
+++ application-back/src/routes/postsRoutes.js
@@ -1,8 +1,18 @@
 import express from "express";
 import multer from "multer";
-import { listarPosts, postarNovoPost } from "../controllers/postsController.js";
+import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
 
-const upload = multer({dest: "./uploads"})
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ dest: "./uploads", storage })
 
 const routes = (app) => {
     app.use(express.json());
