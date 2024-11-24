873ebdebd1c990aeb47fb3eb3228101efca2d4be
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-24 17:25:04 -0300

    add try and catch

--- application-back/src/controllers/postsController.js
+++ application-back/src/controllers/postsController.js
@@ -8,7 +8,18 @@ export async function listarPosts(req, res) {
 }
 
 export async function postarNovoPost(req, res) {
-    
+
     const novoPost = req.body;
+
+    try {
+
+        const postCriado = await criarPost(novoPost);
+        res.status(200).json(postCriado);
+
+    } catch (error) {
+
+        console.error(error.message);
+        res.status(500).json({ Erro: "Falha na requisição" })
+    }
 
 }
