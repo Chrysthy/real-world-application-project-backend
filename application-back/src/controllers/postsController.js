2e6da69542515726c950704e56cbf904365a85f8
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-24 18:10:18 -0300

    add uploadImage function

--- application-back/src/controllers/postsController.js
+++ application-back/src/controllers/postsController.js
@@ -23,3 +23,20 @@ export async function postarNovoPost(req, res) {
     }
 
 }
+
+export async function uploadImagem(req, res) {
+
+    const novoPost = req.body;
+
+    try {
+
+        const postCriado = await criarPost(novoPost);
+        res.status(200).json(postCriado);
+
+    } catch (error) {
+
+        console.error(error.message);
+        res.status(500).json({ Erro: "Falha na requisição" })
+    }
+
+}
