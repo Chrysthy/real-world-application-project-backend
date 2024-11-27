5aeddc4ce2fcd6038548aea30bb4236b85e5aa02
Chrystine Martins <chrystine.martins@outlook.com>
2024-11-26 21:07:31 -0300

    add const cors

--- application-back/src/routes/postsRoutes.js
+++ application-back/src/routes/postsRoutes.js
@@ -3,6 +3,12 @@ import multer from "multer";
 import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postsController.js";
 import cors from "cors";
 
+const cors = { 
+    
+    origin: "http://localhost:8000",
+    optionsSuccessStatus: 200
+}
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'uploads/');
